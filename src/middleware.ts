a8823ccfe5b9eb197a5070589b8bee04e726ed02
Andres Bonilla <andresbonilla1256@gmail.com>
2024-09-22 17:25:07 -0500

    middleware completey working with i18n and auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,91 +34,97 @@ export default async function middleware(request: NextRequest) {
 
   };
 
-  switch (request.nextUrl.pathname) {
+  const pathnameOficial = request.nextUrl.pathname;
+  const accountRegex = /^\/(en|de|es|fr|zh|hi|ja|ru|pt)\/account$/;
+  const loginRegex = /^\/(en|de|es|fr|zh|hi|ja|ru|pt)\/auth\/login$/;
+  const registerRegex = /^\/(en|de|es|fr|zh|hi|ja|ru|pt)\/auth\/register$/;
 
-        case "/account": {
+  if (accountRegex.test(pathnameOficial)) {
 
-            const cookieToken = request.cookies.get('x-token');
-            const redirectUrl = new URL('/auth/login', request.nextUrl);
+    // in in: /account route
+    const cookieToken = request.cookies.get('x-token');
+    const redirectUrl = new URL('/es/auth/login', request.nextUrl);
 
-            if (!cookieToken) {
-                return NextResponse.redirect(redirectUrl);
-            }
-
-            try {
-                const response = await fetch(`${backendUri}/api/auth/renew-token`, {
-                                                method: 'GET',
-                                                headers: {
-                                                    'Authorization': `Bearer ${cookieToken.value}`
-                                                },
-                                            })
-                if (!response.ok) {
-                    clearCookies(request);
-                    return NextResponse.redirect(redirectUrl);
-                }
-
-                try {
-
-                    const jsonresponse = await response.json();
-
-                    if (!jsonresponse.token) {
-
-                        clearCookies(request);
-                        return NextResponse.redirect(redirectUrl);
-
-                    }else{
-                        
-                        const response = responseWithI18n(request);
-                        response.cookies.set('x-token', jsonresponse.token, {path: '/', maxAge: 60 * 60 * 12});
-                        response.cookies.set('uid', jsonresponse.user.uid.toString(), {path: '/', maxAge: 60 * 60 * 12});
-                        response.cookies.set('email', jsonresponse.user.email.toString(), {path: '/', maxAge: 60 * 60 * 12});
-                        response.cookies.set('username', jsonresponse.user.username.toString(), {path: '/', maxAge: 60 * 60 * 12});
-                        response.cookies.set('plan-type', jsonresponse.user.planType.toString(), {path: '/', maxAge: 60 * 60 * 12});
-                        response.cookies.set('credits', jsonresponse.user.credits.toString(), {path: '/', maxAge: 60 * 60 * 12});
-                        response.cookies.set('summaries-language', jsonresponse.user.summariesLanguage.toString(), {path: '/', maxAge: 60 * 60 * 12});
-                        return response;
-
-                    }
-                    
-                } catch (error) {
-                    clearCookies(request);
-                    return NextResponse.redirect(redirectUrl);
-                }    
-                
-                
-            } catch (error) {
-                clearCookies(request);
-                return NextResponse.redirect(redirectUrl);
-            }
-            
-        }
-        case "/auth/login": {
-            const redirectUrl = new URL('/account', request.url);
-            const res = await validateIfAuthenticated(request);
-            if (res) {
-                return responseWithI18n(request);
-            }else{
-                return NextResponse.redirect(redirectUrl);
-            }
-        }
-        case "/auth/register": {
-            const redirectUrl = new URL('/account', request.url);
-            const res = await validateIfAuthenticated(request);
-            if (res) {
-                return responseWithI18n(request);
-            }else{
-                return NextResponse.redirect(redirectUrl);
-            }
-        }
-        default: {
-            return responseWithI18n(request);
-        }
-        
+    if (!cookieToken) {
+        return NextResponse.redirect(redirectUrl);
     }
+
+    try {
+      
+        const response = await fetch(`${backendUri}/api/auth/renew-token`, {
+                                        method: 'GET',
+                                        headers: {
+                                            'Authorization': `Bearer ${cookieToken.value}`
+                                        },
+                                    })
+        if (!response.ok) {
+            clearCookies(request);
+            return NextResponse.redirect(redirectUrl);
+        }
+
+        try {
+
+          const jsonresponse = await response.json();
+
+          if (!jsonresponse.token) {
+
+              clearCookies(request);
+              return NextResponse.redirect(redirectUrl);
+
+          }else{
+                        
+              const response = responseWithI18n(request);
+              response.cookies.set('x-token', jsonresponse.token, {path: '/', maxAge: 60 * 60 * 12});
+              response.cookies.set('uid', jsonresponse.user.uid.toString(), {path: '/', maxAge: 60 * 60 * 12});
+              response.cookies.set('email', jsonresponse.user.email.toString(), {path: '/', maxAge: 60 * 60 * 12});
+              response.cookies.set('username', jsonresponse.user.username.toString(), {path: '/', maxAge: 60 * 60 * 12});
+              response.cookies.set('plan-type', jsonresponse.user.planType.toString(), {path: '/', maxAge: 60 * 60 * 12});
+              response.cookies.set('credits', jsonresponse.user.credits.toString(), {path: '/', maxAge: 60 * 60 * 12});
+              response.cookies.set('summaries-language', jsonresponse.user.summariesLanguage.toString(), {path: '/', maxAge: 60 * 60 * 12});
+              return response;
+
+          }
+                    
+          } catch (error) {
+              clearCookies(request);
+              return NextResponse.redirect(redirectUrl);
+          }    
+                
+                
+      } catch (error) {
+          clearCookies(request);
+          return NextResponse.redirect(redirectUrl);
+      };
+
+  };
+
+  if (loginRegex.test(pathnameOficial)) {
+      // in in: /auth/login route
+      const redirectUrl = new URL('/es/account', request.url);
+      const res = await validateIfAuthenticated(request);
+      if (res) {
+          return responseWithI18n(request);
+      }else{
+          return NextResponse.redirect(redirectUrl);
+      };
+  };
+
+  if (registerRegex.test(pathnameOficial)) {
+      // in in: /auth/register route
+      const redirectUrl = new URL('/es/account', request.url);
+      const res = await validateIfAuthenticated(request);
+      if (res) {
+          return responseWithI18n(request);
+      }else{
+          return NextResponse.redirect(redirectUrl);
+      };
+  };
+
+  return responseWithI18n(request);
   
 }
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(en|de|es|fr|zh|hi|ja|ru|pt)/:path*']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
