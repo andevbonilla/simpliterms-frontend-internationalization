969c73294f5c51e0f0022e244174385c6b2bbad7
Andres Bonilla <andresbonilla1256@gmail.com>
2024-09-30 01:04:02 -0500

    updates in tye price boxes

--- src/components/pages/PricePage.tsx
+++ src/components/pages/PricePage.tsx
@@ -2,11 +2,42 @@
 import { PriceBox } from '../ui/PriceBox';
 
 export const PriceWraper = () => {
+
+        const buyMonthAccess = () => {
+
+        };
+
+        const buyYearAccess = () => {
+
+        };
+
         return (
-                <>
-                        <PriceBox />
-                        <PriceBox />
-                </>
+                <div className='md:flex-row md:px-4 flex flex-col items-center justify-center'>
+                        <PriceBox
+                                actionButton={buyMonthAccess}
+                                price={"0"}
+                                textMonth={"month"}
+                                characteristics={[
+                                        "Privacy Policy Summaries for one month",
+                                        "Terms of use Summaries for one month",
+                                        "24/7 customer support"
+                                ]}
+                                title={"1-Month Access"}
+                                desc={"Just one click on the extension will generate a very short automatic summary of the policies of the page you are on."}
+                        />
+                        <PriceBox
+                                actionButton={buyYearAccess}
+                                price={"0"}
+                                textMonth={"month"}
+                                characteristics={[
+                                        "Privacy Policy Summaries for one year",
+                                        "Terms of use Summaries for one year",
+                                        "24/7 customer support"
+                                ]}
+                                title={"12-Month Access"}
+                                desc={"Just one click on the extension will generate a very short automatic summary of the policies of the page you are on."}
+                        />
+                </div>
         )
 }
 
--- src/components/ui/PriceBox.tsx
+++ src/components/ui/PriceBox.tsx
@@ -1,113 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/router';
-import { toast } from 'react-toastify';
 import Cookies from 'js-cookie';
-import axios from 'axios';
+import { Bebas_Neue } from 'next/font/google';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-export const PriceBox = () => {
+const bebas = Bebas_Neue({
+    weight: ["400"], // bold de la fuente
+    subsets: ["latin"],
+    display: 'swap'
+});
 
-    const notifyError = (message: string) => toast.error(message);
-    const notifySuccess = (message: string) => toast.success(message);
 
-    const [userInfo, setuserInfo] = useState({
-        token: '',
-        username: '',
-        email: '',
-        uid: '',
-        planType: ''
-    })
-    const router = useRouter();
-    const [waiting, setWaiting] = useState(true);
+interface priceBoxInterface {
+    actionButton: any,
+    price: string,
+    textMonth: string,
+    characteristics: String[],
+    title: string,
+    desc: string
+}
 
-    useEffect(() => {
-
-        setuserInfo({
-            token: Cookies.get('x-token') || '',
-            username: Cookies.get('username') || '',
-            email: Cookies.get('email') || '',
-            uid: Cookies.get('uid') || '',
-            planType: Cookies.get('plan-type') || ''
-        });
-
-        setTimeout(() => {
-            setWaiting(false);
-        }, 200);
-
-    }, []);
-
-    const goToLogin = () => {
-        router.push('/auth/login');
-    }
-
-    const freeSubscription = async () => {
-
-        try {
-            const { data } = await axios.put(`${backendUri}/api/payment/create-free`, {}, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${userInfo.token}`
-                }
-            });
-
-            if (data.status === "success") {
-                notifySuccess("You have successfully subscribed to the free plan");
-                Cookies.set("plan-type", data.userDBUpdated.planType, { path: '/account' });
-                Cookies.set("plan-type", data.userDBUpdated.planType, { path: '/' });
-                setuserInfo({
-                    ...userInfo,
-                    planType: data.userDBUpdated.planType
-                });
-            }
-
-        } catch (error) {
-            notifyError("Could not subscribe to the free plan, try again");
-        }
-
-    }
+export const PriceBox = ({
+    actionButton,
+    price,
+    textMonth,
+    characteristics,
+    title,
+    desc
+}: priceBoxInterface) => {
 
     return (
-        <>
-            <div>
+        <div className='md:mx-4'>
 
-                <div className='p-6 rounded border-black border-opacity-40 border-solid border-[1px]'>
+            <div className='p-6 rounded border-black border-opacity-40 border-solid border-[1px]'>
 
-                    <div className="flex flex-col items-center text-center">
-                        <h2 className={`font-bold text-[1.75rem] ${bebas.className}`}>{type}</h2>
-                        <p className="text-sm my-4 mx-4 border-[#5712DF] py-2 px-4 rounded-full border-solid border-2 border-opacity-20">{desc}</p>
-                        <h3 className=" font-extrabold text-3xl mt-4">{price} /<span className="text-lg">{month}</span></h3>
-                    </div>
-
-
-                    <ul className="px-2 py-6 mb-4">
-                        {
-                            characteristics.map((char: any, index: any) => (
-                                <li className="flex pt-3" key={index}>
-                                    <FontAwesomeIcon width={20} height={20} className="text-green-400 mr-2 pt-2" icon={faCheck} />
-                                    <p>{char}</p>
-                                </li>
-
-                            ))
-                        }
-                    </ul>
-
-                    {
-                        (type.toString().toLowerCase() === userInfo.planType.toString().toLowerCase()) && <button className="hover:bg-[#6324DF] transition-all bg-[#5712DF] w-full text-white py-2 mt-4 rounded">You are already subscribed</button>
-                    }
-
-                    {
-                        (!waiting && !(type.toString().toLowerCase() === userInfo.planType.toString().toLowerCase())) && <FreePayButton planType={userInfo.planType}
-                            token={userInfo.token}
-                            goToLogin={goToLogin}
-                            freeSubscription={freeSubscription}
-                        />
-                    }
-
-
+                <div className="flex flex-col items-center text-center">
+                    <h2 className={`font-bold text-[1.75rem] ${bebas.className}`}>
+                        {title}
+                    </h2>
+                    <p className="text-sm my-4 mx-4 border-[#5712DF] py-2 px-4 rounded-full border-solid border-2 border-opacity-20">
+                        {desc}
+                    </p>
+                    <h3 className=" font-extrabold text-3xl mt-4">{price} /<span className="text-lg">{textMonth}</span></h3>
                 </div>
 
-            </div>
-        </>
 
+                <ul className="px-2 py-6 mb-4">
+                    {
+                        characteristics.map((char: any, index: any) => (
+                            <li className="flex pt-3" key={index}>
+                                <FontAwesomeIcon
+                                    width={20}
+                                    height={20}
+                                    className="text-green-400 mr-2 pt-2" icon={faCheck}
+                                />
+                                <p>{char}</p>
+                            </li>
+
+                        ))
+                    }
+                </ul>
+
+
+                <button
+                    className="hover:bg-[#6324DF] transition-all bg-[#5712DF] w-full text-white py-2 mt-4 rounded"
+                >
+                    Buy
+                </button>
+
+            </div>
+
+        </div>
     )
 }
